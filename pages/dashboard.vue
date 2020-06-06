0a0292df20feca6d873ca9e45a1c008d860f02d1
xWafl <73sampleperson@gmail.com>
2020-06-06 06:00:27 -0400

    :sparkles: Dashboard page

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,18 +1,75 @@
 <template>
     <v-layout>
-        {{ userData }}
+        <v-container>
+            <v-row>
+                <v-card v-if="loaded" style="clear: both">
+                    <v-card-text>
+                        <p>
+                            <span class="display-3 text--primary">
+                                {{ userData.username }}
+                            </span>
+                            <span
+                                class="display-2"
+                                :style="{
+                                    color: roleToColor(userData.role),
+                                    fontWeight: 'bold'
+                                }"
+                            >
+                                {{ userData.role.charAt(0).toUpperCase() }}
+                            </span>
+                            <span>
+                                {{ userData.role.slice(1) }}
+                            </span>
+                        </p>
+                    </v-card-text>
+                </v-card>
+            </v-row>
+            <v-row>
+                <v-expansion-panels v-if="loaded" multiple>
+                    <v-expansion-panel>
+                        <v-expansion-panel-header>
+                            E-mail
+                        </v-expansion-panel-header>
+                        <v-expansion-panel-content>
+                            {{ userData.email }}
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                    <v-expansion-panel>
+                        <v-expansion-panel-header>
+                            Discord Key
+                        </v-expansion-panel-header>
+                        <v-expansion-panel-content>
+                            Not supported!
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                    <v-expansion-panel>
+                        <v-expansion-panel-header>
+                            Random row
+                        </v-expansion-panel-header>
+                        <v-expansion-panel-content>
+                            Just something here for testing.
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </v-row>
+        </v-container>
     </v-layout>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from "vue";
+
+type UserRole = "admin" | "member" | "verified" | "none";
+
 export default Vue.extend({
     name: "Dashboard",
     data: () => ({
-        userData: null
+        userData: null,
+        loaded: false
     }),
     async mounted() {
         this.userData = await this.getUserData();
+        this.loaded = true;
     },
     methods: {
         async getUserData() {
@@ -26,7 +83,18 @@ export default Vue.extend({
                     }
                 }
             );
-            return await resp.json();
+            return (await resp.json()).user;
+        },
+        roleToColor(role: UserRole) {
+            if (role === "admin") {
+                return "#cc2900";
+            } else if (role === "member") {
+                return "#00800f";
+            } else if (role === "verified") {
+                return "#003580";
+            } else {
+                return "#777";
+            }
         }
     }
 });
