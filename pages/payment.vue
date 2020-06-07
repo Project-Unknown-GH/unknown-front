f93990bba9e409a7fd64c9dca25e9d02cc24bfb3
xWafl <73sampleperson@gmail.com>
2020-06-07 17:11:09 -0400

    :sparkles: Payment system

--- pages/payment.vue
+++ pages/payment.vue
@@ -1,59 +1,73 @@
 <template>
-    <div id="payment">
-        <h2>Payment details:</h2>
-        <card
-            class="stripe-card"
-            :class="{ complete }"
-            :stripe="config.stripeKey"
-            :options="config.stripeOptions"
-            @change="complete = $event.complete"
-        />
-        <button class="pay-with-stripe" :disabled="!complete" @click="pay">
-            Pay with credit card
-        </button>
+    <div>
+        <v-snackbar top v-model="errored">
+            {{ errorMessage }}
+            <v-btn color="pink" text @click="errored = false">
+                Close
+            </v-btn>
+        </v-snackbar>
+        <stripe-elements
+            ref="elementsRef"
+            :pk="publishableKey"
+            :amount="amount"
+            @token="tokenCreated"
+            @loading="loading = $event"
+        >
+        </stripe-elements>
+        <button @click="submit">Pay ${{ amount / 100 }}</button>
     </div>
 </template>
 
 <script>
-import { Card, createToken } from "vue-stripe-elements-plus";
-import config from "@/assets/config.ts";
-
+import { StripeElements } from "vue-stripe-checkout";
+import config from "@/assets/config";
 export default {
-    components: { Card },
-    data() {
-        return {
-            complete: false,
-            config
-        };
+    components: {
+        StripeElements
     },
-
+    data: () => ({
+        loading: false,
+        amount: 1000,
+        publishableKey: "pk_test_1SMbb3HOTJRaOp9Cpy8iAg9K00hW9hlE7T",
+        token: null,
+        charge: null,
+        errored: false,
+        errorMessage: ""
+    }),
     methods: {
-        async pay() {
-            // createToken returns a Promise which resolves in a result object with
-            // either a token or an error key.
-            // See https://stripe.com/docs/api#tokens for the token object.
-            // See https://stripe.com/docs/api#errors for the error object.
-            // More general https://stripe.com/docs/stripe.js#stripe-create-token.
-            const data = await createToken();
-            console.log(data.token);
+        submit() {
+            this.$refs.elementsRef.submit();
+        },
+        tokenCreated(token) {
+            this.token = token;
+            // for additional charge objects go to https://stripe.com/docs/api/charges/object
+            this.charge = {
+                source: token.id,
+                currency: "usd",
+                amount: this.amount, // the amount you want to charge the customer in cents. $100 is 1000 (it is strongly recommended you use a product id and quantity and get calculate this on the backend to avoid people manipulating the cost)
+                description: this.description // optional description that will show up on stripe when looking at payments
+            };
+            this.sendTokenToServer(this.charge);
+        },
+        async sendTokenToServer(token) {
+            const resp = await fetch(`${config.serverUrl}/api/payment/pay`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    stripeToken: token
+                })
+            });
+            const status = await resp.json();
+            if (status.status !== 200) {
+                this.errored = true;
+                this.errorMessage = status.message;
+            } else {
+                await this.$router.push("/dashboard");
+            }
         }
     }
 };
 </script>
-
-<style>
-.stripe-card {
-    width: 300px;
-    color: #fff;
-    border: solid white;
-    border-width: 0 0 1px 0;
-    padding: 10px 10px 10px 8px;
-    transition: border 0.5s;
-}
-.stripe-card:active {
-    border: solid teal;
-}
-.stripe-card.complete {
-    border-color: green;
-}
-</style>
