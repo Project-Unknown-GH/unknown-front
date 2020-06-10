03adf40685692362e7c6e4759cfc4d048c50e8bf
xWafl <73sampleperson@gmail.com>
2020-06-10 18:08:23 -0400

    :bug: Fix bug with config url

--- assets/config.ts
+++ assets/config.ts
@@ -1,5 +1,5 @@
 export default {
-    serverUrl: "http://159.89.187.6:7400/",
+    serverUrl: "http://159.89.187.6:7400",
     stripeKey: "pk_test_1SMbb3HOTJRaOp9Cpy8iAg9K00hW9hlE7T",
     stripeOptions: {}
 };
